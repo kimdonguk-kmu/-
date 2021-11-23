abe5e8698f268ae615f0e8a746e56afbd2d7f796
kimdonguk-kmu <90365268+kimdonguk-kmu@users.noreply.github.com>
2021-11-23 15:53:26 +0900

    Create 21_bbc_20212963.ino

---
+++ 21_bbc_20212963.ino
@@ -0,0 +1,85 @@
+#include <Servo.h>
+
+// Arduino pin assignment
+#define PIN_SERVO 10
+#define PIN_IR A0
+#define PIN_LED 9
+
+// configurable parameters
+#define _DUTY_MIN 1000 // servo full clockwise position (0 degree)
+#define _DUTY_NEU 1475 // servo neutral position (90 degree)
+#define _DUTY_MAX 2000 // servo full counterclockwise position (180 degree)
+
+#define _POS_START (_DUTY_MIN + 100)
+#define _POS_END (_DUTY_MAX - 100)
+
+#define _SERVO_SPEED 60 // servo speed limit (unit: degree/second)
+#define INTERVAL 10  // servo update interval
+
+// global variables
+unsigned long last_sampling_time; // unit: ms
+int duty_chg_per_interval; // maximum duty difference per interval
+int toggle_interval, toggle_interval_cnt;
+float pause_time; // unit: sec
+Servo myservo;
+int duty_target, duty_curr;
+unsigned long cur_time;
+
+float a = 69.42;
+float b = 253.12;
+
+void setup() {
+// initialize GPIO pins
+  myservo.attach(PIN_SERVO); 
+  duty_target = duty_curr = _POS_START;
+  myservo.writeMicroseconds(1475);
+  pinMode(PIN_LED,OUTPUT);
+  digitalWrite(PIN_LED, 1);
+  
+// initialize serial port
+  Serial.begin(57600);
+
+// convert angle speed into duty change per interval.
+  duty_chg_per_interval = (float)(_DUTY_MAX - _DUTY_MIN) * _SERVO_SPEED / 180 * INTERVAL / 1000;
+
+// initialize variables for servo update.
+  pause_time = 1;
+  toggle_interval = (180.0 / _SERVO_SPEED + pause_time) * 1000 / INTERVAL;
+  toggle_interval_cnt = toggle_interval;
+  
+// initialize last sampling time
+  last_sampling_time = 0;
+}
+
+float ir_distance(void){ 
+  float val;
+  float volt = float(analogRead(PIN_IR));
+  val = ((6762.0/(volt-9.0))-4.0) * 10.0;
+  return val;
+}
+
+void loop() {
+// wait until next sampling time. 
+// millis() returns the number of milliseconds since the program started. Will overflow after 50 days.
+  if(millis() < last_sampling_time + INTERVAL) return;
+  
+  float raw_dist = ir_distance();
+  float dist_cali = 100 + 300.0 / (b - a) * (raw_dist - a);
+
+// adjust duty_curr toward duty_target by duty_chg_per_interval
+  if(dist_cali > 255) {
+    myservo.writeMicroseconds(1425);
+  }
+  else {
+    myservo.writeMicroseconds(1600);
+  }
+  
+  Serial.print("min:0,max:500,dist:");
+  Serial.print(raw_dist);
+  Serial.print(",dist_cali:");
+  Serial.println(dist_cali);
+  delay(20);
+
+// update last sampling time
+  last_sampling_time += INTERVAL;
+}
